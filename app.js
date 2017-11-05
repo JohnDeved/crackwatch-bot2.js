e49c6b14abb17f1f61931d50122778c982c847cb
DevSec <arma.devsec@gmail.com>
2017-11-05 16:51:17 +0100

    - legacy code

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// const irc = require('irc')
 const text2png = require('text2png')
 const imgurUploader = require('imgur-uploader')
 const Snoowrap = require('snoowrap')
@@ -20,7 +19,6 @@ require('colors')
 
 const r = new Snoowrap(CONFIG.snoowrap['0'])
 const rstorm = new Snoostorm(r)
-// const ircclient = new irc.Client(CONFIG.irc.server, CONFIG.irc.nickname, CONFIG.irc.options)
 const igdbClient = igdb(CONFIG.igdb.apiKey)
 
 program.version('0.1.0')
@@ -99,8 +97,6 @@ const checkNfo = (release, count) => {
     imgurPost(release, release => {
       if (release.imgur) {
         release.text = redditText(release)
-        // console.log('Updating Post'.green, release.title.grey)
-        // r.getSubmission(release.submission.name).edit(release.text)
         console.log('Updating Comment'.green, release.submission.id.grey)
         r.getComment(release.submission.id).edit(release.text)
       } else {
@@ -111,8 +107,6 @@ const checkNfo = (release, count) => {
           text = redditText(release)
           if (release.text !== text) {
             release.text = text
-            // console.log('Updating Post'.green, release.title.grey)
-            // r.getSubmission(release.submission.name).edit(release.text)
             console.log('Updating Comment'.green, release.submission.id.grey)
             r.getComment(release.submission.id).edit(release.text)
           }
@@ -143,32 +137,13 @@ const checkNfo = (release, count) => {
 }
 
 const redditPost = release => {
-  // release.benchmark = (Date.now() - release.date) / 1000
-  // console.info(release.name.grey, 'done in'.grey, release.benchmark, 'sec'.grey)
   release.text = redditText(release)
   r.getSubreddit(CONFIG.subreddit[CONFIG.mode])
-  // .submitSelfpost({
-  //   title: (release.os ? `[${release.os}] ` : '') + release.title,
-  //   text: release.text
-  // })
   r.getSubmission(release.post.id)
   .reply(release.text)
   .then(submission => {
-    // console.log(submission)
     release.submission = submission
     console.info('Posted on Reddit'.green, release.submission.id.grey)
-
-    // if (CONFIG.mode === 'live') {
-    //   r.getSubmission(release.submission.name)
-    //   .getLinkFlairTemplates()
-    //   .then(flairs => {
-    //     let flair = flairs.find(e => e.flair_text === 'Release')
-    //     if (flair.flair_template_id) {
-    //       r.getSubmission(release.submission.name)
-    //       .selectFlair({flair_template_id: flair.flair_template_id})
-    //     }
-    //   })
-    // }
 
     if (!release.imgur) {
       setTimeout(() => {
@@ -207,7 +182,6 @@ const imgurPost = (release, callback) => {
 }
 
 const finalize = release => {
-  // if (CONFIG.sections.indexOf(release.section) !== -1) {
   igdbClient.games({
     search: release.name,
     fields: '*',
@@ -215,8 +189,6 @@ const finalize = release => {
   }).then(response => {
     if (response.body.length !== 0) {
       release.igdb = response.body[0]
-      // if (!release.igdb.popularity) { return console.log('game isnt popular enough! [NULL]'.red, release.igdb.url) }
-      // if (release.igdb.popularity < 1.5) { return console.log(`game isnt popular enough! [${release.igdb.popularity}]`.red, release.igdb.url) }
       console.log('igdb'.green, release.igdb.url.grey)
       imgurPost(release, redditPost)
     } else {
@@ -226,75 +198,8 @@ const finalize = release => {
     console.error(error)
     imgurPost(release, redditPost)
   })
-  // } else {
-  //   console.error('wrong section? you must be debugging'.red, release.section)
-  //   imgurPost(release, redditPost)
-  // }
 }
 
-// const precheck = (from, to, message) => {
-//   message = message.replace(/[\x02\x1F\x0F\x16]|\x03(\d\d?(,\d\d?)?)?/g, '')
-//   if (/^\[NFO\]- /.test(message)) { return console.log(message.grey) }
-//   if (/^\[NUKE\]/.test(message)) { return console.log(message.red) }
-//   if (/^\[UNNUKE\]/.test(message)) { return console.log(message.yellow) }
-//   if (!/\[PRE\] \[?(.+)\] ((.+)-(.+))/.test(message)) { return /* console.error('msg syntax error:'.red, message) */ }
-//   let [, section, title, name, group] = message.match(/\[PRE\] \[?(.+)\] ((.+)-(.+))/)
-
-//   let release = {
-//     section: section,
-//     title: title,
-//     name: name,
-//     group: group.toUpperCase(),
-//     date: Date.now()
-//   }
-
-//   if (/\.Linux|\.MacOSX/.test(release.name)) {
-//     if (/\.MacOSX/.test(release.name)) {
-//       release.os = 'MacOSX'
-//     } else {
-//       release.os = 'Linux'
-//     }
-//     release.name = release.name.replace(/\.Linux|\.MacOSX/, '')
-//   }
-
-//   console.log('Pre:'.grey, section.grey, release.name.grey, release.group.grey)
-
-//   if (CONFIG.groups.indexOf(release.group) !== -1) {
-//     console.log('Release found!'.green, release.group)
-//     if (CONFIG.sections.indexOf(release.section) !== -1) {
-//       console.log('Section:'.green, release.section)
-//       if (!/UPDATE/i.test(release.title)) {
-//         finalize(release)
-//       } else {
-//         console.log('Updates are not Allowed!'.red)
-//       }
-//     } else {
-//       console.log('Disallowed Section:'.red, section)
-//     }
-//   } else {
-//     if (CONFIG.sections.indexOf(release.section) !== -1) {
-//       if (/assassin.*creed.*origins/i.test(release.title)) {
-//         console.log('Important Release found!'.green, release.group)
-//         if (!/UPDATE/i.test(release.title)) {
-//           finalize(release)
-//         } else {
-//           console.log('Updates are not Allowed!'.red)
-//         }
-//       }
-//     } else {
-//       if (CONFIG.done) { return }
-//       if (CONFIG.mode === 'debug' && program.test) {
-//         CONFIG.done = true
-//         finalize(release)
-//       }
-//     }
-//   }
-// }
-// ircclient.addListener('error', message => console.log('irc error: '.red, message))
-// ircclient.addListener('registered', msg => console.log('Connected to', msg.server.green))
-// ircclient.addListener('message', precheck)
-
-// if (CONFIG.mode === 'debug' && !program.test) { precheck(CONFIG.irc.sender, CONFIG.irc.channel, CONFIG.test[Math.floor(Math.random() * CONFIG.test.length)]) }
 console.log('Mode:', CONFIG.mode.green, 'Subreddit:', CONFIG.subreddit[CONFIG.mode], 'Reddit-User:', CONFIG.snoowrap['0'].username)
 
 var submissionStream = rstorm.SubmissionStream({
@@ -303,7 +208,6 @@ var submissionStream = rstorm.SubmissionStream({
 })
 
 submissionStream.on('submission', function (post) {
-  // console.log(post)
   console.log(`New submission by ${post.author.name}: ${post.title}`.grey)
   if (/REPACK/i.test(post.title)) { return }
   if (/KaOs/i.test(post.title)) { return }
